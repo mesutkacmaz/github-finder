86ba9013b9deeeb07ef2df11e1415518dfb68994
mesutkacmaz <mesut.kacmaz@outlook.com>
2022-08-02 22:53:34 +0300

    Added user search functionality

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,30 +14,42 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading()
 
-    const res = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await res.json()
+    const { items } = await res.json()
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
+
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
